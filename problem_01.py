3da0b1c10c1976cfbe9c3b0a6e37c3a58e6977f2
johnrey32 <bulfajohnrey_bsit@plmun.edu.ph>
2025-02-09 21:20:26 +0800

    Submitting Python Session 03 - Exercise 01

--- problem_01.py
+++ problem_01.py
@@ -4,6 +4,7 @@
 # Simple Calculator Program
 
 def main():
+    
     print("Simple Calculator Program")
     num1 = int(input("Enter 1st number: "))
     num2 = int(input("Enter 2nd number: "))
--- problem_02.py
+++ problem_02.py
@@ -1,9 +1,10 @@
-# John Rey 
+# John Rey Bulfa 
 # ITELEC2
 # Problem Set 01 - Problem 02
 # Square the Number Program
 
 def main():
+    
     print("Square the Number Program")
     number = int(input("Enter a number: "))
     print(f"The square of {number} is {(number*number):.2f}")
